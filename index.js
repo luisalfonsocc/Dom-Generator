5942cab6f016817acb8e158663e6a5c5f52de958
Luis Carreno <luisalfonsocc@gmail.com>
2023-10-04 18:36:10 -0300

    Segundo Cambio

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 let pronoun = ['the', 'our'];
 let adj = ['great' , 'big'];
 let noun = ['jogger' , 'racoon'];
-
+const domain = ".com"
 
 for(let pronombre of pronoun){
     for(let adjetivo of adj){
         for(let nombre of noun){
-            console.log([pronombre] +  [adjetivo] +  [nombre] +".com");    
+            console.log([pronombre] +  [adjetivo] +  [nombre] + domain);    
         }
     }
     
